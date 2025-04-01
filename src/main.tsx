642e0794b27b0a30ea0ea3551a9aa0d45d3179e5
Rodrigobs4 <rpdrogpbs4@gomail.com>
2025-04-01 01:19:53 -0300

    route login

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/painel" element={<Painel />} />
         <Route path="/planejamento" element={<CadastroPlanejamento />} />
         <Route path="/cadastro-usuario" element={<CadastroUsuario />} />
